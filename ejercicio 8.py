c6f64a147c7a57956acf3de248684e97b6599817
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2022-02-23 17:27:23 +0000

    ejercicio 8

---
+++ ejercicio 8.py
@@ -0,0 +1,16 @@
+import tabulate
+
+def descomponer():
+
+  separador = str(input("Introduce el separador que desea utilizar: "))
+  cadena = str(input("Introduce un texto para analizar: "))
+
+  ca = cadena.split(separador)
+  
+  lista = []
+  for i in range(0, len(ca)):
+    lista.append(list(ca[i:i+1]))
+
+  table = lista
+  headers = ["nº", "Cadena"]
+  print(tabulate.tabulate(table, headers, tablefmt = "fancy_grid", showindex = True))

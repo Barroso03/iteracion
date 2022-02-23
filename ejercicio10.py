530033b59e74dcf2b9044b129f7d2bbf0dc906ee
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2022-02-23 19:25:52 +0000

    ejercicio 10

---
+++ ejercicio10.py
@@ -0,0 +1,37 @@
+class Persona:
+   def __init__(self, identidad, edad, padre, madre):
+    self.identidad = identidad
+    self.edad = edad
+    self.padre = padre
+    self.madre = madre
+
+  def lista_edad_20_30(self):
+    r = []
+    for i in range(numero de personas):
+      if 20 < self.edad < 30:
+        r.append(self.nombre)
+    return r
+
+  def numero_huerfanos(self):
+   
+    r = []
+    for i in range (1000)
+    if self.padre == 0 and self.madre == 0:
+      if self.edad < 15:
+        r.append(self.identidad)
+    return r
+  
+  def envejecer_1_año(self):
+    for i in range (numero de personas):
+      self.edad[i] += 1
+
+  def identidad_padre_jaime(self):
+    if self.identidad = "Jaime MARTIN"
+      return self.padre
+
+if __name__ == "__main__":
+
+  familias = []
+  for i in range (1000):
+    r = Persona(identidad, padre, madre, edad)
+    familias.append(r)
